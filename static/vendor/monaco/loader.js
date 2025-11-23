ae25036a29b34e2c9a7928c0750921ccd6d3fbba
Adnan Kakkattil <adnan@nomadscipher.com>
2025-11-23 14:55:27 +0530

    fdgdfsg

--- static/vendor/monaco/loader.js
+++
@@ -1,7 +0,0 @@
-// Placeholder for Monaco editor loader.
-// Replace with actual Monaco distribution files during front-end asset setup.
-(function () {
-  console.warn("Monaco loader placeholder: please add Monaco editor assets.");
-})();
-
-
