0feab8a893a44df2bff5b381ba93f86d2a9b78aa
Adnan Kakkattil <adnan@nomadscipher.com>
2025-11-23 21:34:43 +0530

    DockerFile

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_secret_key_123'
-    MYSQL_HOST = os.environ.get('MYSQL_HOST') or '127.0.0.1'
-    MYSQL_USER = os.environ.get('MYSQL_USER') or 'root'
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or ''
-    MYSQL_DB = os.environ.get('MYSQL_DB') or 'interview_system'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', '127.0.0.1')
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'interview_system')
     MYSQL_CURSORCLASS = 'DictCursor'
